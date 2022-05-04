dfc501520e543154e26f138eb5f0287d36d4f7f6
SylvainVialatte <sylvain.vialatte@gmail.com>
2022-05-04 18:10:05 +0200

    Patch note 178.4.2.1

--- Convolution.cpp
+++ Convolution.cpp
@@ -70,7 +70,7 @@ void pasAlpha( unsigned char* rgb, unsigned char* g, size_t imgCols,size_t imgRo
                     for (int decalageCol = -limCols; decalageCol < limCols+1; decalageCol++){
                         for (int decalageRow = -limRows; decalageRow < limRows+1; decalageRow++){
 
-                            sum += rgb[3*(( row + decalageRow )*imgCols+( col + decalageCol ))+i] * noyau.matrice[ decalageRow + limRows ][ decalageCol + limCols ];//coefficient de la matrice de convolution à l'indice associé, on fait la rotation en même temps par le calcul d'indice
+                            sum += rgb[3*(( row + decalageCol )*imgCols+( col + decalageRow ))+i] * noyau.matrice[ decalageRow + limRows ][ decalageCol + limCols ];//coefficient de la matrice de convolution à l'indice associé, on fait la rotation en même temps par le calcul d'indice
                             cout << "decalage row :" << decalageRow << " decalageCol" << decalageCol << " coefficient de la matrice" << noyau.matrice[ decalageRow + limRows ][ decalageCol + limCols ] << endl;
                             cout << "coordonnees de la matrice (" << decalageRow + limRows << "," << decalageCol + limCols << ")" <<endl;
                         }
