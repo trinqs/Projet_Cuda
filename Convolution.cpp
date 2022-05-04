93e224aa78efde2f8ca25ad0d68ec6b14a386612
SylvainVialatte <sylvain.vialatte@gmail.com>
2022-05-04 15:54:31 +0200

    Patch note 178.4.0.5

--- Convolution.cpp
+++ Convolution.cpp
@@ -123,15 +123,16 @@ int main(int n, char* params[])
 
     uchar* g = new uchar[ 3*(rows * cols)]();
 
-    vector<vector<int>> matriceBlur1= { {1,1,1} , {1,1,1} , {1,1,1} };
-    vector<vector<int>> matriceBlur2= { {1,1,1,1,1} , {1,1,1,1,1} , {1,1,1,1,1}, {1,1,1,1,1}, {1,1,1,1,1} };
-
-    matriceConvolution matriceBlurS = matriceConvolution(
+    matriceConvolution matriceBlur1 = matriceConvolution(
         vector<vector<int>>({ {1,1,1} , {1,1,1} , {1,1,1} })
     );
 
+    matriceConvolution matriceBlur3 = matriceConvolution(
+        vector<vector<int>>({ {1,1,1,1,1} , {1,1,1,1,1} , {1,1,1,1,1}, {1,1,1,1,1}, {1,1,1,1,1} })
+    );
+
     if(sizeRGB%3==0){
-        pasAlpha(rgb,g,cols,rows, matriceBlurS);
+        pasAlpha(rgb,g,cols,rows, matriceBlur3);
     }
     if(sizeRGB%4==0){
         //de l'alpha
