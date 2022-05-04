ba642651a822002abe9a89468119646b3272a4e0
SylvainVialatte <sylvain.vialatte@gmail.com>
2022-05-04 18:00:48 +0200

    Patch note 178.4.2.1

--- Convolution.cpp
+++ Convolution.cpp
@@ -71,7 +71,7 @@ void pasAlpha( unsigned char* rgb, unsigned char* g, size_t imgCols,size_t imgRo
                         for (int decalageRow = -limRows; decalageRow < limRows+1; decalageRow++){
 
                             sum += rgb[3*(( row + decalageRow )*imgCols+( col + decalageCol ))+i] * noyau.matrice[ decalageRow + limRows ][ decalageCol + limCols ];//coefficient de la matrice de convolution à l'indice associé, on fait la rotation en même temps par le calcul d'indice
-                            cout << "decalage row :" << decalageRow << " decalageCol" << decalageCol << " coefficient de la matrice" << noyau.matrice[ decalageRow + limRows ][ decalageCol + limCols ] << endl;
+                            //cout << "decalage row :" << decalageRow << " decalageCol" << decalageCol << " coefficient de la matrice" << noyau.matrice[ decalageRow + limRows ][ decalageCol + limCols ] << endl;
                         }
                     }
                     //normalisation en dehors de la boucle pour faire moins d'arrondis
