1873297be599bf91d3bccce625c1747d6e9043bd
Reffay Thomas <thomas.reffay@etu.univ-orleans.fr>
2022-05-09 13:15:08 +0200

    on tente autre chose avant les exam....

--- ConvolutionCuda.cu
+++ ConvolutionCuda.cu
@@ -86,7 +86,7 @@ __global__ void pasAlpha(unsigned char* rgb, unsigned char* g, size_t imgCols,si
     int tidy = blockIdx.y;
     int tidx = threadIdx.y;
 
-    printf("%d \n", tidx);
+    printf(" x :%d , y: %d \n", tidx, tidy);
 
     // si c'est pas un bord
     if( tidy >= limCols && tidy< imgCols-limCols && tidx >= limRows && tidy < imgRow-limRows){
