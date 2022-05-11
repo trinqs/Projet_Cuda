6b29379b26e74eca3662545b89aab72ad88a5d6a
Reffay Thomas <thomas.reffay@etu.univ-orleans.fr>
2022-05-11 14:31:13 +0200

    on print plein de truc

--- ConvolutionCuda.cu
+++ ConvolutionCuda.cu
@@ -78,7 +78,7 @@ __device__ unsigned char calculPixel(int x, int y, // le thread,
 }
 
 __global__ void pasAlpha(unsigned char* rgb, unsigned char* g, int imgCol, int imgRow, matriceConvolution noyau){
-    printf("Dans le kernel, on comprend R, nb ligne : %d , nb cols : %d\n",imgRow, imgCol);
+    //printf("Dans le kernel, on comprend R, nb ligne : %d , nb cols : %d\n",imgRow, imgCol);
     int limCols = noyau.getCols()/2;
     int limRows = noyau.getRows()/2;
 
